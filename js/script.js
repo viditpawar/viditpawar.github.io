bdfa289a47d5b946849ed21e2f0211dac251aca7
Vidit Pawar <vidit.pawar25@gmail.com>
2025-12-13 17:36:19 -0700

    Initial portfolio website

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log("Vidit Pawar portfolio loaded");
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 100;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
